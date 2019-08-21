4538e6896519f0918736e5154b2c596882f0620d
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-08-21 13:07:03 -0300

    Eventos de touch adicionados

--- js/slide.js
+++ js/slide.js
@@ -18,30 +18,43 @@ export default class Slide {
   } 
 
   onStart(event) {
-    event.preventDefault();
-    this.distance.startX = event.clientX;
-    this.wrapper.addEventListener('mousemove', this.onMove);
+    let moveType;
+    if(event.type === 'mousedown') {
+      event.preventDefault();
+      this.distance.startX = event.clientX;
+      moveType = 'mousemove'
+      // console.log(event)
+    } else {
+      this.distance.startX = event.changedTouches[0].clientX;
+      moveType = 'touchmove'
+      // console.log(event)
+    }
+    this.wrapper.addEventListener(moveType, this.onMove);
     // console.log(event)
     // console.log(this)
     // console.log('mouseDown');
   }
   
   onMove() {
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
     // console.log(this.distance.startX - event.clientX)
     // console.log('move');
   }
 
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+    const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
+    this.wrapper.removeEventListener(moveType, this.onMove);
     this.distance.finalPosition = this.distance.movePosition;
      // console.log('end');
   }
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('touchstart', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
+    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
   bindEvents() {

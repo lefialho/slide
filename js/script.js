d2e5fffcbff8d9c70cf17703f7391b538a395fb7
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-08-22 12:50:29 -0300

    Navegação prev e next finalizada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-import Slide from './slide.js';
+import {SlideNav} from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
+slide.addArrow('.prev', '.next');
 
-slide.changeSlide(1);
-slide.activePrevSlide();
-slide.activeNextSlide();
+console.log(slide);
+// slide.changeSlide(1);
+// slide.activePrevSlide();
+// slide.activeNextSlide();

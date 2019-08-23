663e86d546110e8cd3cd8b368fd789e91842d59e
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-08-23 10:15:41 -0300

    Custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-import {SlideNav} from './slide.js';
+import SlideNav from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-slide.addControl()
+slide.addControl('.custom-controls')
 
-console.log(slide);
+// console.log(slide);
 // slide.changeSlide(1);
 // slide.activePrevSlide();
 // slide.activeNextSlide();
--- js/slide.js
+++ js/slide.js
@@ -172,7 +172,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
     this.bindControlEvents();
